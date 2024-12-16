0a581e11543719035f239f9dea535c5d3c560424
ayanokoujifl <guleite3@gmail.com>
2024-12-16 08:45:59 -0300

    funcionarios ok

--- rotas/rotas_funcionario.mjs
+++ rotas/rotas_funcionario.mjs
@@ -1,11 +1,18 @@
-import { Router } from "express";
-import {novo, todos, um, altera, exclui} from "../controles/controle_funcionarios.mjs";
+import { Router } from "express"
+import {
+  novo,
+  todos,
+  um,
+  altera,
+  exclui,
+} from "../controles/controle_funcionarios.mjs"
 
-const rotas_funcionarios = Router();
+const rotas_funcionarios = Router()
 
-rotas_funcionarios.post("/cadastrar", novo); // /cadastrar
-rotas_funcionarios.get("/listar", todos); // /listar
-rotas_funcionarios.put("/editar", altera); // /editar
-rotas_funcionarios.delete("/excluir/:id", exclui); // /excluir
+rotas_funcionarios.post("/cadastrar", novo) // /cadastrar
+rotas_funcionarios.get("/listar", todos) // /listar
+rotas_funcionarios.get("/listar/:id", um) // /listar
+rotas_funcionarios.put("/editar", altera) // /editar
+rotas_funcionarios.delete("/deletar/:id", exclui) // /excluir
 
-export default rotas_funcionarios;
+export default rotas_funcionarios
--- views/Funcionarios/acessa_dados.js
+++ views/Funcionarios/acessa_dados.js
@@ -1,55 +1,55 @@
-import { tbFuncionarios } from "./base_temporaria.js";
-import urlBackEnd from "/constantes/urls.mjs";
+import { tbFuncionarios } from "./base_temporaria.js"
+import urlBackEnd from "/constantes/urls.mjs"
 
 async function getLista() {
-    const resposta = await fetch(urlBackEnd + '/funcionarios/listar');
-    const funcionarios = await resposta.json();
-    return funcionarios;
+  const resposta = await fetch(urlBackEnd + "/funcionarios/listar")
+  const funcionarios = await resposta.json()
+  return funcionarios
 }
 
 async function buscaUm(id) {
-    const resposta = await fetch(urlBackEnd + '/funcionarios/listar/' + id);
-    const funcionario = await resposta.json();
-    return funcionario;
+  const resposta = await fetch(urlBackEnd + "/funcionarios/listar/" + id)
+  const funcionario = await resposta.json()
+  return funcionario
 }
 
 async function novo(obj) {
-    const opt = {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(obj)
-    };
-    const resposta = await fetch(urlBackEnd + '/funcionarios/cadastrar', opt);
-    const cadastrado = await resposta.json();
-    return cadastrado;
+  const opt = {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  }
+  const resposta = await fetch(urlBackEnd + "/funcionarios/cadastrar", opt)
+  const cadastrado = await resposta.json()
+  return cadastrado
 }
 
 async function altera(obj) {
-    const opt = {
-        method: 'put',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(obj)
-    };
-    const resposta = await fetch(urlBackEnd + '/funcionarios/editar', opt);
-    const editado = await resposta.json();
-    return editado;
+  const opt = {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  }
+  const resposta = await fetch(urlBackEnd + "/funcionarios/editar", opt)
+  const editado = await resposta.json()
+  return editado
 }
 
 async function exclui(id) {
-    const opt = {
-        method: 'delete',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id: id })
-    };
-    const resposta = await fetch(urlBackEnd + '/funcionarios/deletar'+ id, opt);
-    const apagado = await resposta.json();
-    return apagado;
+  const opt = {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: id }),
+  }
+  const resposta = await fetch(urlBackEnd + "/funcionarios/deletar/" + id, opt)
+  const apagado = await resposta.json()
+  return apagado
 }
 
-export { getLista, buscaUm, novo, altera, exclui };
+export { getLista, buscaUm, novo, altera, exclui }
